1b1f31b0150f70f7127b54d954deba1fe7fbd742
MatheusPeloso <matheus.peloso.souza@gmail.com>
2026-06-02 12:51:35 -0300

    Todas as funções feitas

--- Motor.py
+++ Motor.py
@@ -1,3 +1,4 @@
+import random
 def fazer_pergunta(dados_da_pergunta):
     print(f"\n[{dados_da_pergunta['categoria'].upper()} - {dados_da_pergunta['dificuldade'].capitalize()}]")
     print(dados_da_pergunta['enunciado'])
@@ -22,3 +23,32 @@ def fazer_pergunta(dados_da_pergunta):
     opcao_certa = dados_da_pergunta['opcoes'][dados_da_pergunta['correta']]
     print(f"Errado! A resposta correta era: {opcao_certa}")
     return False
+
+def jogar(lista_perguntas):
+    pontos = 0
+    total = 0
+    perguntas_aleatorias = lista_perguntas.copy()
+    random.shuffle(perguntas_aleatorias)
+    
+    print("\n=== O JOGO VAI COMEÇAR ===")
+    
+    for pergunta in perguntas_aleatorias:
+        total += 1
+        if fazer_pergunta(pergunta):
+            pontos += 1
+        else:
+            print("\nGame Over! Você errou a questão.")
+            break
+            
+    print(f"\nPartida encerrada. Pontuação final: {pontos}")
+  
+
+def mostrar_resultado(pontos, total):
+    print("\n==============================")
+    print("         FIM DE JOGO!         ")
+    print("==============================")
+    print(f"Você acertou {pontos} de {total} perguntas.")
+    
+    if total > 0:
+        aproveitamento = (pontos / total) * 100
+        print(f"Desempenho: {aproveitamento:.1f}%")
